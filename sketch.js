9ea7540e44f3abe32056b56e6cbc9c39a1d961f4
gabrielchi <55122175+gabrielchi@users.noreply.github.com>
2020-04-29 22:17:10 +0800

    light  test

--- sketch.js
+++ sketch.js
@@ -1,5 +1,27 @@
+angle = 0;
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    fill("black");
-    rect(0, 0, width, height);
+createCanvas(400, 300, WEBGL);
+}
+
+function draw(){
+pointLight(0, 0, 255, 200, 0, 0);
+pointLight(255, 0, 0, -200, 0, 0);
+
+background(175);
+
+
+//translate(0,0, mouseX);
+rotateX(angle);
+rotateY(angle * 0.3);
+rotateZ(angle * 1.2);
+
+
+
+//fill('green');
+noStroke();
+ambientMaterial(255);
+sphere(100);
+angle += 0.03;
+
 }
