3a2e070a7d27d6e8f000c711823b7d515051f2d4
gabrielchi <55122175+gabrielchi@users.noreply.github.com>
2020-05-04 21:07:54 +0800

    test2

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function preload(){
     pic = loadImage('test.jpg');
     //cam = createCapture(VIDEO);
     //cam.hide();
+    //threedee = loadModel('');
 }
 
 function setup() {
@@ -13,33 +14,39 @@ function setup() {
 }
 
 function draw(){
-    //pointLight(0, 0, 255, 200, 0, 0);
-    //pointLight(255, 0, 0, -200, 0, 0);
-    ambientLight(255);
+    // let camX = map(mouseX, 0, width, -200, 200);
+    // camera(camX, 0, (height/2) / tan(PI/6), camX, 0, 0, 0, 1, 0);
+
+    // //pointLight(0, 0, 255, 200, 0, 0);
+    // //pointLight(255, 0, 0, -200, 0, 0);
+    // ambientLight(255);
+    
+    //ortho(-200, 200, 200, -200, -1000, 1000);
 
     background(175);
     noStroke();
 
+    pointLight(255, 255, 255, 0, -200, 200);
+
+    for (let x = -200; x < 200; x += 50){
     push();
-    //translate(0,0, mouseX);
+    translate(x, 0, x - 200);
     rotateX(angle);
     rotateY(angle * 0.3);
     rotateZ(angle * 1.2);
-
-
-    //fill('green');
-    
-    //ambientMaterial(255);
-    texture(pic);
-    //sphere(100);
-    sphere(100);
+    //texture(pic);
+    ambientMaterial(255);
+    box(50);
+    //model();
     pop();
+    }
 
     translate(0,100);
     rotateX(HALF_PI);
-    ambientMaterial(35);
+
     plane(500, 500);
 
     //speed
     angle += 0.03;
 }
+
